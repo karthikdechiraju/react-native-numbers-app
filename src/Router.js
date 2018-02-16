b5182b548a0d923bf19dbdbbdf1d503fc684782a
Karthik Dechiraju <karthik@pyritetechnologies.com>
2018-02-16 15:09:12 +0530

    made app skeleton with two screens

---
+++ src/Router.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Scene, Router, Actions } from 'react-native-router-flux';
+import Home from './components/Home';
+import Formulae from './components/Formulae';
+
+
+const RouterComponent = () => {
+	return(
+		<Router>
+			<Scene key="numbers">
+				<Scene key="home" component={Home} hideNavBar initial />
+				<Scene key="formulae" component={Formulae} title="Formulae" />
+			</Scene>
+		</Router>
+	)
+};
+
+
+export default RouterComponent;
---
+++ src/components/Formulae.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Formulae extends Component{
+	render(){
+		return(
+			<View>
+				<Text>List of formlae</Text>
+			</View>
+		)
+	}
+}
+
+export default Formulae;
